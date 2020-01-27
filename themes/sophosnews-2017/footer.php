6ded4592bc69479402ce88466878f3032558ccd6
Mark Stockley <mark@compoundeye.co.uk>
2020-01-27 17:36:21 +0000

    Updated footer to match sophos.com

--- themes/sophosnews-2017/footer.php
+++ themes/sophosnews-2017/footer.php
@@ -10,120 +10,46 @@
 
 		</div> <!-- .container -->
 	</div> <!-- #content -->
-
-	<footer class="site-footer" role="contentinfo">
-		<div class="container">
-			<div class="social-footer-links">
-				<?php if ( \Sophos\Region\Menu\exists( 'footer-social' ) ) : ?>
-				    <?php wp_nav_menu([
-						'menu'        => \Sophos\Region\Menu\slug( 'footer-social' ),
-						'menu_class'  => 'social-sharing',
-						'fallback_cb' => '',
-						'container'   => 'ul',
-					]); ?>
-				<?php endif; ?>
-			</div>
-			<div class="site-footer-links">
-				<?php if ( \Sophos\Region\Menu\exists( 'footer-popular' ) ) : ?>
-					<div class="site-footer-title">
-						<?php esc_html_e( 'Popular', 'sophos-news' ) ?>
-					</div>
-					<?php wp_nav_menu([
-						'menu'        => \Sophos\Region\Menu\slug( 'footer-popular' ),
-						'fallback_cb' => '',
-						'container'   => 'ul',
-					]); ?>
-				<?php endif; ?>
-			</div>
-			<div class="site-footer-links">
-				<?php if ( \Sophos\Region\Menu\exists( 'footer-community' ) ) : ?>
-					<div class="site-footer-title">
-						<?php esc_html_e( 'Community', 'sophos-news' ); ?>
-					</div>
-					<?php wp_nav_menu([
-						'menu'        => \Sophos\Region\Menu\slug( 'footer-community' ),
-						'fallback_cb' => '',
-						'container'   => 'ul',
-					]); ?>
-				<?php endif; ?>
-			</div>
-			<div class="site-footer-links">
-				<?php if ( \Sophos\Region\Menu\exists( 'footer-work' ) ) : ?>
-					<div class="site-footer-title">
-						<?php esc_html_e( 'Work With Us', 'sophos-news' ); ?>
-					</div>
-					<?php wp_nav_menu([
-						'menu'        => \Sophos\Region\Menu\slug( 'footer-work' ),
-						'fallback_cb' => '',
-						'container'   => 'ul',
-					]); ?>
-				<?php endif; ?>
-			</div>
-			<div class="site-footer-links">
-				<?php if ( \Sophos\Region\Menu\exists( 'footer-about' ) ) : ?>
-					<div class="site-footer-title">
-						<?php esc_html_e( 'About Sophos', 'sophos-news' ); ?>
-					</div>
-					<?php wp_nav_menu([
-						'menu'        => \Sophos\Region\Menu\slug( 'footer-about' ),
-						'fallback_cb' => '',
-						'container'   => 'ul',
-					]); ?>
-				<?php endif; ?>
-			</div>
-			<div class="site-footer-links">
-				<?php if ( \Sophos\Region\Menu\exists( 'footer-support' ) ) : ?>
-					<div class="site-footer-title">
-						<?php esc_html_e( 'Support', 'sophos-news' ); ?>
-					</div>
-					<?php wp_nav_menu([
-						'menu'        => \Sophos\Region\Menu\slug( 'footer-support' ),
-						'fallback_cb' => '',
-						'container'   => 'ul',
-					]); ?>
-				<?php endif; ?>
-			</div>
-		</div>
-	</footer> <!-- site-footer -->
-
-	<div class="secondary-footer">
-		<div class="container">
-			<div class="site-logo">
-				<?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-			</div>
-			<div class="site-copyright">
-			<?php
-				/* Translators: Copyright statement such as "© 1997 - 2017 Sophos Ltd. All rights reserved". &copy; is turned into the copyright symbol, %1$s and %2$s are turned into start and end dates automatically. */
-				echo esc_html( sprintf( _x( '&copy; %1$s - %2$s Sophos Ltd. All rights reserved', 'Copyright statement in the website footer', 'sophos-news' ), date_i18n( 'Y', 852076800 ), date_i18n( 'Y' ) ) );
-			?>
-			</div>
-			<nav class="site-legal-navigation" role="navigation">
+	<footer>
+		<section class="cta">
+			<p><?php esc_html_e( 'Start a Sophos demo in less than a minute. See exactly how our solutions work in a full environment without a commitment.', 'sophos-news' ); ?></p>
+			<a href="https://secure2.sophos.com/en-us/products/demos.aspx?cmp=70130000001xKqzAAE"><?php esc_html_e( 'Learn More', 'sophos-news' ); ?></a>
+		</section>
+		<section class="connected">
+			<div class="social">
+				<h3><?php esc_html_e( 'Stay Connected', 'sophos-news' ); ?></h3>
 				<ul>
-					<?php wp_nav_menu([
-						'menu'        => \Sophos\Region\Menu\slug( 'footer-legal' ),
-						'fallback_cb' => '',
-						'container'   => 'false',
-					]); ?>
-					<?php if ( function_exists( 'vip_powered_wpcom' ) ) :
-						?><li><?php echo vip_powered_wpcom(); ?></li><?php
-					endif; ?>
+					<li><a href="https://www.facebook.com/securitybysophos" target="_blank"><img src="https://www.sophos.com/en-us/medialibrary/SophosNext/Images/Navigation/Footer/facebook-icon.svg?la=en&amp;hash=1CA0A5A0E9C9999E7E079A5D32C86E9BF9C7AD1E" alt="Facebook"></a>
+					</li>
+					<li><a href="https://www.instagram.com/sophossecurity/" target="_blank"><img src="https://www.sophos.com/en-us/medialibrary/SophosNext/Images/Navigation/Footer/instagram-icon.svg?la=en&amp;hash=2B50C2B67C3023D9E7D4E1C3A16401EAD8F10F76" alt="Instagram"></a>
+					</li>
+					<li><a href="https://www.linkedin.com/company/sophos" target="_blank"><img src="https://www.sophos.com/en-us/medialibrary/SophosNext/Images/Navigation/Footer/linkedin-icon.svg?la=en&amp;hash=B6D5378C1E1A16F239A4C862EAE3499B8EC1F3F9" alt="LinkedIn"></a></li>
+					<li><a href="https://www.sophos.com/en-us/company/rss-feeds.aspx"><img src="https://www.sophos.com/en-us/medialibrary/SophosNext/Images/Navigation/Footer/rss-icon.svg?la=en&amp;hash=76C4DAFD331DC675814191A66794A778D8C6DCF7" alt="RSS"></a></li>
+					<li><a href="https://twitter.com/Sophos" target="_blank"><img src="https://www.sophos.com/en-us/medialibrary/SophosNext/Images/Navigation/Footer/twitter-icon.svg?la=en&amp;hash=9A90D9F99ABE6454741BBAAF9C3FB6966624C92D" alt="Twitter"></a></li>
+					<li><a href="https://www.youtube.com/user/SophosProducts" target="_blank"><img src="https://www.sophos.com/en-us/medialibrary/SophosNext/Images/Navigation/Footer/youtube-icon.svg?la=en&amp;hash=CCCF647AFA78C8DB74D654DB9FEFBF0AC54F42D3" alt="YouTube"></a></li>
 				</ul>
-			</nav>
+			</div>
+			<ul class="links">
+				<li><a href="https://www.sophos.com/en-us/company/careers.aspx"><?php esc_html_e( 'Careers', 'sophos-news' ); ?> </a></li>
+				<li><a href="https://www.sophos.com/en-us/partners/partner-locator.aspx"><?php esc_html_e( 'Find a Partner', 'sophos-news' ); ?></a></li>
+				<li><a href="https://secure2.sophos.com/en-us/support.aspx"><?php esc_html_e( 'Support', 'sophos-news' ); ?></a></li>
+				<li><a href="https://www.sophos.com/en-us/threat-center/technical-papers.aspx"><?php esc_html_e( 'Technical Papers', 'sophos-news' ); ?></a></li>
+				<li><a href="https://www.sophos.com/en-us/security-news-trends/whitepapers.aspx"><?php esc_html_e( 'Whitepapers', 'sophos-news' ); ?></a></li>
+			</ul>
+		</section>
+		<section class="legal">
+			<div class="copyright"><?php echo esc_html( sprintf( _x( '&copy; %1$s - %2$s Sophos Ltd. All rights reserved', 'Copyright statement in the website footer', 'sophos-news' ), date_i18n( 'Y', 852076800 ), date_i18n( 'Y' ) ) ); ?></div>
+			<ul>
+				<li><a href="https://www.sophos.com/en-us/legal.aspx"><?php esc_html_e( 'Legal', 'sophos-news' ); ?></a></li>
+				<li><a href="https://www.sophos.com/en-us/legal/sophos-group-privacy-policy.aspx"><?php esc_html_e( 'Privacy', 'sophos-news' ); ?></a></li>
+				<li><a href="https://www.sophos.com/en-us/legal/cookie-information.aspx"><?php esc_html_e( 'Cookie Information', 'sophos-news' ); ?></a></li>
+				<li><a href="https://www.sophos.com/en-us/legal/modern-slavery-act-transparency-statement.aspx"><?php esc_html_e( 'Modern Slavery Statement', 'sophos-news' ); ?></a></li>
+			</ul>
+		</section>
+		<div class="vip" style="background: #002939">
+			<?php echo vip_powered_wpcom(); ?>
 		</div>
-	</div> <!-- secondary-footer -->
-
-</div> <!-- #page -->
-
-<?php if ( is_front_page() || is_tax( 'region' ) ) : ?>
-	<div class="mobile-footer">
-		<ul>
-			<li class="active"><a href="#latest">Latest</a></li>
-			<li><a href="#featured">Featured</a></li>
-		</ul>
-	</div>
-<?php endif; ?>
-
-<?php wp_footer(); ?>
-
+	</footer>
+	<?php wp_footer(); ?>
 </body>
 </html>

b2de3759797373e0595eb1b1d75ed43a589e9d58
Mark Stockley <mark@compoundeye.co.uk>
2020-01-29 13:00:43 +0000

    Fix for "PHP Warning:  array_rand(): Array is empty"

--- plugins/ad/widget/class-random.php
+++ plugins/ad/widget/class-random.php
@@ -17,8 +17,11 @@ add_filter( 'sidebars_widgets', function ( $sidebars_widgets ) {
 	$sidebar = \Sophos\Ad\Shortcode::$sidebar;
 	if ( ! is_admin() && is_array( $sidebars_widgets ) && array_key_exists( $sidebar, $sidebars_widgets ) ) {
 		$ads 	 = $sidebars_widgets[ $sidebar ];
-		$index	 = array_rand( $ads, 1 );
-		$sidebars_widgets[ $sidebar ] = [ $ads[ $index ] ];
+
+		if ( ! empty( $ads ) ) {
+			$index	 = array_rand( $ads, 1 );
+			$sidebars_widgets[ $sidebar ] = [ $ads[ $index ] ];
+		}
 	}
 
 	return $sidebars_widgets;

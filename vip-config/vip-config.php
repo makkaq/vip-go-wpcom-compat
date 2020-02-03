d6bb43b01b7be1e24d0aff52e2f4935f8f819d68
Mark Stockley <mark@compoundeye.co.uk>
2020-02-03 20:47:45 +0000

    Setup redirects for legacy domains

--- vip-config/vip-config.php
+++ vip-config/vip-config.php
@@ -15,3 +15,63 @@
  *
  * - The WordPress.com VIP Team
  **/
+
+namespace Sophos\URL {
+
+    /**
+     * Redirect legacy Sophos News domains
+     */
+    function redirect_legacy_domains () {
+
+        $canonical_host = 'news.sophos.com';
+        $request_uri    = filter_input( INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL );
+        $http_host      = filter_input( INPUT_SERVER, 'HTTP_HOST', FILTER_CALLBACK, [
+            'options' => function ( $domain ) {
+                if ( in_array( $domain, [
+                    'sophosbenelux.com',
+                    'www.sophosbenelux.com',
+                    'sophosbenelux.be',
+                    'www.sophosbenelux.be',
+                    'blog.sophos.be',
+                    'sophosblog.de',
+                    'www.sophosblog.de',
+                    'blog.sophos.de',
+                    'sophosfranceblog.fr',
+                    'www.sophosfranceblog.fr',
+                    'blog.sophos.fr',
+                    'sophositalia.it',
+                    'www.sophositalia.it',
+                    'sophositalia.com',
+                    'www.sophositalia.com',
+                    'blog.sophos.it',
+                    'sophosiberia.es',
+                    'www.sophosiberia.es'
+                ], true ) ) {
+                    return $domain;
+                }
+
+                return false;
+            }
+        ]);
+
+        // Don't redirect if the host check failed
+        if ( false === $http_host ) {
+            return false;
+        }
+
+        // Don't redirect for '/cache-healthcheck?' or monitoring will break
+        if ( '/cache-healthcheck?' === $request_uri ) {
+            return false;
+        }
+
+        // Don't redirect in WP CLI context
+        if ( ( defined( 'WP_CLI' ) && WP_CLI ) ) {
+            return false;
+        }
+
+        header( 'Location: https://' . $canonical_host . $request_uri, true, 301 );
+        exit;
+    }
+
+    redirect_legacy_domains();
+};
